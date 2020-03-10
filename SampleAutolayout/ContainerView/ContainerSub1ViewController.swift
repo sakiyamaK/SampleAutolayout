cdc3e6d2821e4abce854168eaac7b38e64b6357a
sakiyamaK <sakiyama.k@gmail.com>
2020-03-11 02:44:46 +0900

    a

---
+++ SampleAutolayout/ContainerView/ContainerSub1ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContainerSub1ViewController.swift
+//  SampleAutolayout
+//
+//  Created by sakiyamaK on 2020/03/11.
+//  Copyright © 2020 sakiyamaK. All rights reserved.
+//
+
+import UIKit
+
+class ContainerSub1ViewController: UIViewController {
+    
+}
---
+++ SampleAutolayout/ContainerView/ContainerSub2ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ContainerSub2ViewController.swift
+//  SampleAutolayout
+//
+//  Created by sakiyamaK on 2020/03/11.
+//  Copyright © 2020 sakiyamaK. All rights reserved.
+//
+
+import UIKit
+
+class ContainerSub2ViewController: UIViewController {
+
+}
+
