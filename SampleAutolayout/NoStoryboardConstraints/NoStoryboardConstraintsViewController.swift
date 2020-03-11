bcb8d3b6cd7e795f2ad55dfe2a3266b3e1ad2c01
sakiyamaK <sakiyama.k@gmail.com>
2020-03-12 01:03:45 +0900

    a

---
+++ SampleAutolayout/NoStoryboardConstraints/NoStoryboardConstraintsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ConstraintsViewController.swift
+//  SampleAutolayout
+//
+//  Created by sakiyamaK on 2020/03/11.
+//  Copyright © 2020 sakiyamaK. All rights reserved.
+//
+
+import UIKit
+
+
+class ConstraintsViewController: UIViewController {
+    
+}
